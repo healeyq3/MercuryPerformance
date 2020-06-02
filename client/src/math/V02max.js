ecf826e8a33fbd6bd651d5014b796293ff254079
Quill Healey <healeyq3@me.com>
2020-06-02 11:49:30 -0400

    getWorkoutPace available

--- client/src/math/V02max.js
+++ client/src/math/V02max.js
@@ -1,5 +1,17 @@
-v02max = (data) => {
-    data.distance = getDistance(data)
+function v02max(data) {
+    const distance = getDistance(data);
+    const time = getTime(data);
+    const perMile = getPerMile(distance, time);
+}
+
+function getWorkoutPace(data){
+    const distance = getDistance(data);
+    const time = getTime(data);
+    const perMile = getPerMile(distance, time);
+    const pMax = getPercentMax(time);
+    const workoutPace = perMile * pMax;
+    const toReturn = getPaceString(workoutPace);
+    return toReturn;
 }
 
 function getDistance(data){
@@ -18,4 +30,48 @@ function convertKToM(distance){
 
 function convertMeToMi(distance) {
     return (distance / 1609);
+}
+
+function getTime(data){
+    const time = 0;
+    if(data.hours != null){
+        time += (data.hours * 3600);
+    }
+    if(data.minutes != null){
+        time += (data.minutes * 60);
+    }
+    if(data.seconds != null){
+        time += data.seconds
+    }
+    return time;
+}
+
+function getPerMile(distance, time){
+    const perMile = (time / distance);
+    return perMile;
+}
+
+function getPercentMax(time){
+    const e = getE();
+    const pMax = .8 + .1894393 * (Math.pow(e, (-.012778*time))) + .2989558 * (Math.pow(e, (-.1932605 * time)));
+    return pMax;
+}
+
+function getE(){
+    return Math.E;
+}
+
+function getPaceString(seconds){
+    const initialMinutes = seconds / 60;
+    const minutes = Math.trunc(seconds / 60);
+    const minutesAnswer = minutes.toString();
+    const remainingSeconds = Math.trunc(seconds - (60 * minutes));
+    const remainingSecondsAnswer = remainingSeconds.toString();
+    if((seconds - (60 * initialMinutes)) == 0){
+        return minutesAnswer + ":00"
+    } else if(remainingSeconds < 10 && remainingSeconds != 0){
+        return minutesAnswer + ":0" + remainingSecondsAnswer;
+    } else {
+        return minutesAnswer + ":" + remainingSecondsAnswer;
+    }
 }
