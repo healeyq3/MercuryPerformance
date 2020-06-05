71fe186107cbe60ab21f6631269eb9e447874c9b
patsfanasg <aidansgannon@icloud.com>
2020-06-05 11:30:06 -0400

    Combined reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import teamReducer from './teamReducer';
 import runnerReducer from './runnerReducer';
+import eventReducer from './eventReducer';
 
 export default combineReducers({
   teams: teamReducer,
-  runners: runnerReducer
+  runners: runnerReducer,
+  events: eventReducer
 });
