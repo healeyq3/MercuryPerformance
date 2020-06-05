30385a62951e0e15bdea0cb1c38f1fb64bdf6a54
neirbonhoj <43376907+neirbonhoj@users.noreply.github.com>
2020-06-05 10:55:17 -0400

    put back runnerbackend

--- runnerBackend.js
+++ runnerBackend.js
@@ -24,4 +24,31 @@ router.post('/',async (req, res) => {
   });
 });
 
+router.post('/new', async (req, res) => {
+  let authenticationSuccess = true;
+  await firebaseUtils.authenticatePost(req, res).then((success) => {
+    authenticationSuccess = success;
+  })
+  if(!authenticationSuccess){
+    res.end();
+    return;
+  }
+
+  const data = req.body;
+  const name = data.runnerData.runnerName;
+  const email = data.runnerData.runnerEmail;
+  const experience = data.runnerData.runnerExperience;
+  const gradYear = data.runnerData.runnerGradYear;
+  const wPace = data.runnerData.runnerWorkoutPace;
+
+  firebaseUtils.createRunner(req.session.user, data.selectedTeamUID, name, email, experience, gradYear, wPace).then((runners) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(runners));
+  }).catch((error) => {
+    console.log("Error adding and fetching Runners".red);
+    console.log(error);
+    res.end("{}");
+  })
+});
+
 module.exports = router;
