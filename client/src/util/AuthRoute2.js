1b0e63d827321ad1be84e5c2c3701d210ac65ab4
Quill Healey <healeyq3@me.com>
2020-06-06 17:52:51 -0400

    adding to AuthRoute2 for teamSelect verification

--- client/src/util/AuthRoute2.js
+++ client/src/util/AuthRoute2.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
 
 const AuthRoute2 = ({component: Component, authenticated, ...rest}) => (
     <Route
     {...rest}
     render = {(props) => 
-        authenticated == null ? <Redirect to = '/login'/> : <Component { ...props} />
+        authenticated == null || this.props.selectedTeam == null ? <Redirect to = '/login'/> : <Component { ...props} />
     }
     />
 );
-   
 
-export default AuthRoute2
+
+AuthRoute2.propTypes = {
+    selectedTeam: PropTypes.string
+}
+
+const mapStateToProps = function(state) {
+    return {
+        selectedTeam: state.teams.selectedTeam
+    }
+}
+
+export default connect(mapStateToProps, {}) (AuthRoute2)
