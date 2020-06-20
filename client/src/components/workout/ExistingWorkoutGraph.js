43b948055d004ddf0cc3a1635b6377b654e693a1
patsfanasg <aidansgannon@icloud.com>
2020-06-20 14:11:29 -0400

    New component

---
+++ client/src/components/workout/ExistingWorkoutGraph.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import  { Card } from 'react-bootstrap'
+
+export class ExistingWorkoutGraph extends Component {
+    render() {
+        return (
+            <Card>
+                <Card.Header className = "text-center">Graph</Card.Header>
+            </Card>
+        )
+    }
+}
+
+export default ExistingWorkoutGraph
