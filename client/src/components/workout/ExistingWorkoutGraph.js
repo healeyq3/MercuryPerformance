50085d72198685c0ee5101641a02b8e3e5ad5a5a
patsfanasg <aidansgannon@icloud.com>
2020-06-20 14:13:41 -0400

    Test graph

--- client/src/components/workout/ExistingWorkoutGraph.js
+++ client/src/components/workout/ExistingWorkoutGraph.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import  { Card } from 'react-bootstrap'
+import { VictoryBar, VictoryChart } from 'victory';
 
 export class ExistingWorkoutGraph extends Component {
     render() {
+        const data = [
+            {quarter: 1, earnings: 13000},
+            {quarter: 2, earnings: 16500},
+            {quarter: 3, earnings: 14250},
+            {quarter: 4, earnings: 19000}
+          ];
         return (
             <Card>
                 <Card.Header className = "text-center">Graph</Card.Header>
+                <Card.Body>
+                <VictoryChart domainPadding={20}>
+                    <VictoryBar
+                    data={data}
+                    x="quarter"
+                    y="earnings"
+                    />
+                </VictoryChart>
+                </Card.Body>
             </Card>
         )
     }
