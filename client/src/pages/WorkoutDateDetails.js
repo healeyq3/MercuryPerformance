fe5948136f4f1670436273b388da12842257dfa3
patsfanasg <aidansgannon@icloud.com>
2020-06-21 10:33:35 -0400

    New page

--- client/src/pages/WorkoutDateDetails.js
+++ client/src/pages/WorkoutDateDetails.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import { Col, Row, Card, Form } from 'react-bootstrap'
+import WorkoutDateRunnerCard from '../components/workout/WorkoutDateRunnerCard'
 
 export class WorkoutDateDetails extends Component {
     render() {
+        
         return (
-            <div>
-                
-            </div>
+            <Row>
+            <Col>
+                <Card className = "text-center">
+                    <Card.Header>Going</Card.Header>
+                    <Card.Body>
+                        <WorkoutDateRunnerCard></WorkoutDateRunnerCard>
+                        <WorkoutDateRunnerCard></WorkoutDateRunnerCard>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card className = "text-center">
+                    <Card.Header>Not Going</Card.Header>
+                    <Card.Body>
+                        <WorkoutDateRunnerCard></WorkoutDateRunnerCard>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card className = "text-center">
+                    <Card.Header>Notes</Card.Header>
+                    <Form>
+                      <Form.Control as ="textarea">
+                      </Form.Control>
+                    </Form>
+                </Card>
+            </Col>
+            </Row>
         )
     }
 }
