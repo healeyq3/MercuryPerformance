e3909f0d10ef4a1f62855bd59ac9df98a5f142c5
patsfanasg <aidansgannon@icloud.com>
2020-06-22 16:33:15 -0400

    Merged

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {addWorkoutToTeam, getAllWorkoutBlueprints, getWorkoutBlueprints, setBlueprint} from '../actions/workoutActions';
 import ImportWorkoutDropdown from "../components/workout/ImportWorkoutDropdown";
-import { getWorkoutBlueprints, setBlueprint } from '../actions/workoutActions';
 import { Redirect } from "react-router-dom";
 
 export class Workouts extends Component {
