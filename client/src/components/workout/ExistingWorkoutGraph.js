b983dfdbbd7956392959af21214ddeb81bff29cc
Quill Healey <healeyq3@me.com>
2020-06-22 12:45:51 -0400

    Commits

--- client/src/components/workout/ExistingWorkoutGraph.js
+++ client/src/components/workout/ExistingWorkoutGraph.js
@@ -1,29 +1,51 @@
-import React, { Component } from 'react'
-import  { Card } from 'react-bootstrap'
-import { VictoryArea, VictoryChart } from 'victory';
+import React, { Component } from 'react';
+import {Row, Col, Card, Button} from 'react-bootstrap'
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    VerticalGridLines,
+    HorizontalGridLines,
+    VerticalRectSeries
+  } from 'react-vis';
 
 export class ExistingWorkoutGraph extends Component {
     render() {
-        const data = [
-            {x: 0, y: 30, fill:'#c43b55'},
-            {x: 2, y: 30, fill:'#c43b55'},
-            {x: 2, y: 80, fill:'#c43b55'},
-            {x: 4, y: 100, fill:'#c43b55'}
-          ];
+        const timestamp = new Date('May 23 2017').getTime();
+        const ONE_DAY = 86400000;
+        
+        const DATA = [
+          {x0: 0, x: 10, y: 67},
+          {x0: 10, x: 20, y: 96},
+          {x0: 20, x: 27, y: 99},
+          {x0: 27, x: 77, y: 50},
+        ].map(el => ({x0: el.x0 , x: el.x, y: el.y}));
         return (
             <Card>
                 <Card.Header className = "text-center">Graph</Card.Header>
                 <Card.Body>
-                <VictoryChart domainPadding={20}> 
-                <VictoryArea
-                    style={{ data: { fill: data[0].fill} }}
-                    data={data}
-                />
-                </VictoryChart>
+                <XYPlot
+        xDomain={[0, 100]}
+        yDomain={[0, 100]}
+        // xType="time"
+        width={300}
+        height={300}
+      >
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        <VerticalRectSeries data={DATA} style={{stroke: '#fff'}} />
+      </XYPlot>
                 </Card.Body>
             </Card>
         )
     }
 }
+
+
+  
+
+
 
 export default ExistingWorkoutGraph
