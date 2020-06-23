450401af0ac1c973cb592305b76bb36361e9e598
patsfanasg <aidansgannon@icloud.com>
2020-06-23 18:16:47 -0400

    Rest works

--- client/src/components/workout/RepDistancePopover.js
+++ client/src/components/workout/RepDistancePopover.js
@@ -12,6 +12,11 @@ export class RepDistancePopover extends Component {
             percent: 0,
             distance: 0,
             distanceUnit:'',
+            restDistance: 0,
+            restDistanceUnit: undefined,
+            restHours:0,
+            restMinutes:0,
+            restSeconds:0,
             reps:1,
             show: false
         }
@@ -30,7 +35,30 @@ export class RepDistancePopover extends Component {
             distanceUnit:this.state.distanceUnit
         }
         for(var i= 0; i<this.state.reps; i++){
+            if(this.state.restDistance!==0){
+                const restData = {
+                    type: this.state.type,
+                    percent:1,
+                    distance:this.state.restDistance,
+                    distanceUnit:this.state.restDistanceUnit
+                }
+                this.props.addArr(repData)
+                this.props.addArr(restData)
+            }
+            else if(this.state.restDuration!==0){
+                const restData = {
+                    type:"rest",
+                    percent: 1,
+                    hours: this.state.restHours,
+                    minutes: this.state.restMinutes,
+                    seconds:this.state.restSeconds,
+                }
+                this.props.addArr(repData)
+                this.props.addArr(restData)
+            }
+            else{
             this.props.addArr(repData)
+            }
         }
     }
     render() {
@@ -67,6 +95,58 @@ export class RepDistancePopover extends Component {
                                     name = "reps"
                                     onChange = {this.handleChange}
                                             />
+                                    <Form.Label>Rest</Form.Label>
+                                    <Row>
+                                        <Col>
+                                    <Form.Control
+                                    inline
+                                    type = "text"
+                                    placeholder = "Distance"
+                                    name = "restDistance"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                    <Form.Control
+                                    inline
+                                    as = "select"
+                                    placeholder = "Unit"
+                                    name = "restDistanceUnit"
+                                    onChange = {this.handleChange}
+                                            ><option hidden>Units</option>
+                                            <option>Miles</option>
+                                            <option>Kilometers</option>
+                                            <option>Meters</option></Form.Control>
+                                            </Col>
+                                            </Row>
+                                            <p>or</p>
+                                    <Row>
+                                        <Col>
+                                    <Form.Control
+                                    type = "text"
+                                    placeholder = "Hours"
+                                    name = "restHours"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                            <Form.Control
+                                    type = "text"
+                                    placeholder = "Minutes"
+                                    name = "restMinutes"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                            <Form.Control
+                                    type = "text"
+                                    placeholder = "Seconds"
+                                    name = "restSeconds"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            </Row>
+                                            <p></p>
                                 <Row>
                                 <Col>
                                 <Button variant = "outline-secondary" onClick={() => this.setState({show: !this.state.show})}>Cancel</Button>
--- client/src/components/workout/RepDurationPopover.js
+++ client/src/components/workout/RepDurationPopover.js
@@ -14,6 +14,11 @@ export class RepDurationPopover extends Component {
             minutes:0,
             seconds:0,
             reps:1,
+            restDistance: 0,
+            restDistanceUnit: undefined,
+            restHours:0,
+            restMinutes:0,
+            restSeconds:0,
             show: false
         }
         this.handleChange = this.handleChange.bind(this);
@@ -32,7 +37,30 @@ export class RepDurationPopover extends Component {
             seconds:this.state.seconds,
         }
         for(var i= 0; i<this.state.reps; i++){
+            if(this.state.restDistance!==0){
+                const restData = {
+                    type: this.state.type,
+                    percent:1,
+                    distance:this.state.restDistance,
+                    distanceUnit:this.state.restDistanceUnit
+                }
+                this.props.addArr(repData)
+                this.props.addArr(restData)
+            }
+            else if(this.state.restDuration!==0){
+                const restData = {
+                    type:"rest",
+                    percent: 1,
+                    hours: this.state.restHours,
+                    minutes: this.state.restMinutes,
+                    seconds:this.state.restSeconds,
+                }
+                this.props.addArr(repData)
+                this.props.addArr(restData)
+            }
+            else{
             this.props.addArr(repData)
+            }
         }
     }
     render() {
@@ -74,6 +102,58 @@ export class RepDurationPopover extends Component {
                                     name = "reps"
                                     onChange = {this.handleChange}
                                             />
+                                    <Form.Label>Rest</Form.Label>
+                                    <Row>
+                                        <Col>
+                                    <Form.Control
+                                    inline
+                                    type = "text"
+                                    placeholder = "Distance"
+                                    name = "restDistance"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                    <Form.Control
+                                    inline
+                                    as = "select"
+                                    placeholder = "Unit"
+                                    name = "restDistanceUnit"
+                                    onChange = {this.handleChange}
+                                            ><option hidden>Units</option>
+                                            <option>Miles</option>
+                                            <option>Kilometers</option>
+                                            <option>Meters</option></Form.Control>
+                                            </Col>
+                                            </Row>
+                                            <p>or</p>
+                                    <Row>
+                                        <Col>
+                                    <Form.Control
+                                    type = "text"
+                                    placeholder = "Hours"
+                                    name = "restHours"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                            <Form.Control
+                                    type = "text"
+                                    placeholder = "Minutes"
+                                    name = "restMinutes"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            <Col>
+                                            <Form.Control
+                                    type = "text"
+                                    placeholder = "Seconds"
+                                    name = "restSeconds"
+                                    onChange = {this.handleChange}
+                                            />
+                                            </Col>
+                                            </Row>
+                                            <p></p>
                                 <Row>
                                 <Col>
                                 <Button variant = "outline-secondary" onClick={() => this.setState({show: !this.state.show})}>Cancel</Button>
