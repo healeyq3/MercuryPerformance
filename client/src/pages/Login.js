7b62a0ad7caaf27cacf3889e1ee59e94076e3530
neirbonhoj <43376907+neirbonhoj@users.noreply.github.com>
2020-06-24 18:34:04 -0400

    adding animation for invalid login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,10 +3,9 @@ import {Link, Redirect, withRouter} from 'react-router-dom';
 import fire from '../Fire';
 import cookie from 'react-cookies'
 import '../css/login.css';
-import logo from "../resources/mLogoV2.svg"
+import stdlogo from "../resources/mLogoV2.svg"
 import envelope from "../resources/mEnvelope.svg"
 import lock from "../resources/mLock.svg"
-//Bootstrap
 import {Form, Card, Container} from 'react-bootstrap';
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -14,18 +13,22 @@ import Row from "react-bootstrap/Row";
 class Login extends Component {
     constructor(props){
         super(props);
+
         this.login = this.login.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.failedLoginReset = this.failedLoginReset.bind(this);
+
         this.state = {
             mercury_email: '',
             password: '',
-            gotoTeamSelect: false
+            gotoTeamSelect: false,
+            failedLogin: false,
         }
     }
 
     handleChange = (e) => {
         this.setState({
-           [e.target.name]: e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
@@ -50,9 +53,21 @@ class Login extends Component {
                     gotoTeamSelect: true
                 })
             })
-        }).catch((error) => {
-            console.log(error);
+        }).catch(() => {
+            this.setState({
+                failedLogin: true,
+                failedLoginResetInterval:
+                    setInterval(this.failedLoginReset, 1200)
+            })
         });
+    }
+
+    failedLoginReset(){
+        console.log("yes");
+        clearInterval(this.state.failedLoginResetInterval);
+        this.setState({
+            failedLogin: false
+        })
     }
 
     render() {
@@ -60,55 +75,60 @@ class Login extends Component {
             return <Redirect to='/teamselect'/>
         }
 
+        let loginButtonClasses = ["login-submit-button"];
+        if(this.state.failedLogin){
+            loginButtonClasses.push("failed-login")
+        }
+
         let disableLoginButton;
         this.state.mercury_email.length > 0 && this.state.password.length > 0 && (this.state.mercury_email.indexOf("@") < this.state.mercury_email.indexOf(".")) ? disableLoginButton = false : disableLoginButton = true;
 
         return (
             <Container className = "login-container">
-                <Card className = "login-card-style">
-                    <div className="login-logo-container">
-                    </div>
-                    <img className="login-logo" src = {logo} alt="logo"/>
-                    <h1 className="login-card-header">Member Login</h1>
-                    <Form className = "login-form">
-                        <Col>
-                            <Row className="justify-content-center mb-1">
-                                <input
-                                    className="form-input-login align-self-center"
-                                    type="email"
-                                    placeholder="Email"
-                                    onChange={this.handleChange}
-                                    value={this.state.email}
-                                    name="mercury_email"/>
-                                <img src={envelope} alt="envelope" className="login-envelope"/>
-                            </Row>
-                            <Row className="justify-content-center">
-                                <input
-                                    className="form-input-login align-self-center"
-                                    type="password"
-                                    placeholder="Password"
-                                    onChange={this.handleChange}
-                                    value={this.state.password}
-                                    name="password"
-                                    id="login-password-input"
-                                />
-                            <img src={lock} alt="lock" className="login-lock"/>
-                            </Row>
-                            <Row className="justify-content-center">
-                                <button
-                                    className="login-submit-button"
-                                    onClick = {(e) => this.login(e)}
-                                    type = 'submit'
-                                    disabled={disableLoginButton}>LOGIN
-                                </button>
-                            </Row>
-                        </Col>
-                    </Form>
+            <Card className = "login-card-style">
+            <div className="login-logo-container">
+            </div>
+            <img className="login-logo" src = {stdlogo} alt="logo"/>
+            <h1 className="login-card-header">Coach Login</h1>
+        <Form className = "login-form">
+            <Col>
+            <Row className="justify-content-center mb-1">
+            <input
+        className="form-input-login align-self-center"
+        type="email"
+        placeholder="Email"
+        onChange={this.handleChange}
+        value={this.state.email}
+        name="mercury_email"/>
+            <img src={envelope} alt="envelope" className="login-envelope"/>
+            </Row>
+            <Row className="justify-content-center">
+            <input
+        className="form-input-login align-self-center"
+        type="password"
+        placeholder="Password"
+        onChange={this.handleChange}
+        value={this.state.password}
+        name="password"
+        id="login-password-input"
+            />
+            <img src={lock} alt="lock" className="login-lock"/>
+            </Row>
+            <Row className="justify-content-center">
+            <button
+        className={loginButtonClasses.join(' ')}
+        onClick = {(e) => this.login(e)}
+        type = 'submit'
+        disabled={disableLoginButton}>LOGIN
+            </button>
+            </Row>
+            </Col>
+            </Form>
 
-                    <Link to = "./signup" className="create-account-button">Create your Account ➔</Link>
-                </Card>
-            </Container>
-        )
+            <Link to = "./signup" className="create-account-button">Create your Account ➔</Link>
+        </Card>
+        </Container>
+    )
     }
 }
 
