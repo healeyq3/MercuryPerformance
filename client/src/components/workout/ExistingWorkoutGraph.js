d2de065a839d6f0f8f9c41b4570c79d18a6eabf0
Quill Healey <healeyq3@me.com>
2020-06-25 10:51:30 -0400

    colors based on V02 functional

--- client/src/components/workout/ExistingWorkoutGraph.js
+++ client/src/components/workout/ExistingWorkoutGraph.js
@@ -20,11 +20,21 @@ export class ExistingWorkoutGraph extends Component {
       let averagePace = this.props.team.hasOwnProperty('averageWPace') === true ? this.props.team.averageWPace : 6.5
       for(const rep in this.props.reps){
         let t = this.props.reps[rep];
+        let c = 0;
+        if(t.percent < 75){
+          c = 0
+        } else if(t.percent < 85){
+          c = 1
+        } else if(t.percent < 95){
+          c = 2
+        } else if(t.percent >= 95){
+          c = 3
+        }
         console.log(t)
         if(t.distanceUnit!==undefined){
           newTime = distanceToTime(t.distance, t.distanceUnit, averagePace /(t.percent/100))
           time += newTime
-          DATA.push({x0:start, x:time, y:t.percent})
+          DATA.push({x0:start, x:time, y:t.percent, color: c})
           
           start +=newTime
         }
@@ -32,12 +42,14 @@ export class ExistingWorkoutGraph extends Component {
           newTime = totalMinutes(t.hours, t.minutes, t.seconds)
           console.log(newTime)
           time +=newTime
-          DATA.push({x0:start, x:time, y:t.percent})
+          DATA.push({x0:start, x:time, y:t.percent, color: 0})
           // DATA.map(el => ({x0: el.x0 , x: el.x, y: el.y}));
           start +=newTime
         }
       }
-      const dataWithColor = DATA.map((d, i) => ({...d, color: 3}))
+      // const dataWithColor = DATA.map((d, i) => ({
+      //   ...d, color: 0
+      //   }))
         return (
           
           <XYPlot
@@ -46,8 +58,9 @@ export class ExistingWorkoutGraph extends Component {
             // xType="time"
             width={700}
             height={400}
-            colorType = "linear"
-            colorDomain = {[0, 1, 2]}
+            // colorType = "linear"
+            colorDomain = {[0, 1, 2, 3]}
+            colorRange = {['green','yellow', 'orange', 'red']}
             
             
           >
@@ -55,7 +68,7 @@ export class ExistingWorkoutGraph extends Component {
             <HorizontalGridLines />
             <XAxis title = "Minutes"/>
             <YAxis title = "V02 Max"/>
-            <VerticalRectSeries data={dataWithColor} style={{stroke: '#fff'}} />
+            <VerticalRectSeries data={DATA} style={{stroke: '#fff'}} />
           </XYPlot>
         )
     }
