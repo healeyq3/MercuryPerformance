763f54a473b4888c87677131112cf85cd8a20898
patsfanasg <aidansgannon@icloud.com>
2020-06-26 15:07:25 -0400

    Accordion works

--- client/src/components/workout/WorkoutRunnerCard.js
+++ client/src/components/workout/WorkoutRunnerCard.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react'
-import { Card, Button, Row, Col } from 'react-bootstrap';
-import timeGenerator from '../../math/TimeConversions'
+import { Card, Button, Row, Col, Accordion } from 'react-bootstrap';
+import {timeGenerator} from '../../math/TimeConversions'
 
 export class WorkoutRunnerCard extends Component {
     render() {
         let time = timeGenerator(this.props.time);
         return (
+            <Accordion defaultActiveKey="0">
             <Card style = {{ height: '10%', orientation: 'horizontal'}}>
-                <Card.Body>
-                    <Row>
+                <Card.Header>
+                <Row>
                         <Col>
-                        <Card.Title>{this.props.runner.name}</Card.Title>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                         {<Card.Title>Name</Card.Title>}
+                        </Accordion.Toggle>
+                        
                         </Col>
-                        <p>{time}</p>
                         <Col></Col>
                         <Col>
                         <Button variant = "outline-primary" onClick = {this.props.setShow}>Edit</Button>
                         <Button variant = "outline-secondary" onClick = {this.handleDelete}>🗑</Button>
                         </Col>
                     </Row>
-                    
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                <Card.Body>Data
                 </Card.Body>
+                </Accordion.Collapse>
             </Card>
+            </Accordion>
         )
     }
 }
